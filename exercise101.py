17ba07a247a6f6724d18945080119f180bd9356e
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-04 22:14:05 +0000

    Completed exercise101 section 1

---
+++ exercise101.py
@@ -0,0 +1,10 @@
+#Input user details
+
+name = 'Lana'
+last_name = 'Smith'
+age = 26
+eye_color = 'Green'
+hair_color = 'Black'
+
+name = input(f'Hello {name}, what new name would you like?: ')
+print("Hello {}! Welcome, your age is {}, your eyes are {} and your hair are {}".format(name, age, eye_color, hair_color))
