3be6d5d61938cd86d6501feb5461d6b905bdc970
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-05 10:00:22 +0000

    Fixed variables

--- exercise101.py
+++ exercise101.py
@@ -7,4 +7,8 @@ eye_color = 'Green'
 hair_color = 'Black'
 
 name = input(f'Hello {name}, what new name would you like?: ')
-print("Hello {}! Welcome, your age is {}, your eyes are {} and your hair are {}".format(name, age, eye_color, hair_color))
+last_name = input(f'what new surname would you like?: ')
+age = input(f'How old would you like to be?: ')
+eye_color = input(f'What eye color would you like?: ')
+hair_color = input(f'What hair color would you like?: ')
+print("Hello {} {}! Welcome, your age is {}, your eyes are {} and your hair are {}".format(name,last_name, age, eye_color, hair_color))
